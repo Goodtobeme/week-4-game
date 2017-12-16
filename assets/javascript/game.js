0066c65d7f4d80262f6d88b9f9ad9439ea4a1d43
Goodtobeme <eugenebeckwith@ymail.com>
2017-12-15 20:19:58 -0600

    Inserted crystals in html, added new css and js to project

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,58 +1,78 @@
 
 
-
-var wins = 0;
-var losses = 0;
-var yourTotalScore = 0;
-var targetNumber = 39;
-
-$( document ).ready(function() {
-var random = [Math.floor(Math.random() * 39)];
-console.log(random);
-// var counter = 0;
-
-// Array with four options
- var numberOptions = [1,10,6,4];
+  var win =0;
+  var loses = 0;
+  var random_result;
+  var previous = 0;
 
 
-// Loop that creates crystals for numberOptions
- for (var i = 0; i < 4; i++) {
-   
-$("#crystalmage0,1,2,3" + i ).attr("data-crystalValue", numberOptions[i]);
-// This iteration creates an image crystal
+  var resetAndStart = function() {
 
-}
+  $(".crystals").empty();
 
-// Giving each crystal a class
+  var images = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRXuZ1yes4PXx6EP7yzdancWNyCkHV49X4yzfHxMnuGCXhSiX2o",
+   "https://www.soulfulcrystals.co.uk/wp-content/uploads/2013/11/a-crystal-healing.jpg",
+   "https://i.pinimg.com/564x/65/4d/e3/654de3967d826661e1da2f907dab2a98--gems-and-minerals-crystals-minerals.jpg",
+   "http://solawakening.com/wp-content/uploads/2011/12/fantasy-crystal-field.jpg"];
 
-// All crystalImage will be given a data attribute called data-crystalValue.
-// This data attribute will be set equal to the array value.
-
-$(".crystal-image1").on("click", function(event) {
-
-// console.log(event.target);
-console.log(event.targetNumber);
-
-// Extract the value from the data-attr
-var crystalValue = ($(this).attr("data-crystalvalue"));
-crystalValue = parseInt(crystalValue);
+  random_number = Math.floor(Math.random() * 29) + 10;
 
 
-counter += crystalValue;
+  $("#result").html("Random Number: " + random_number);
+  $("#previous").html("Previous: " + previous);
+
+  for(var i = 0; i < 4; i++) {
+
+    var random = Math.floor(Math.random() * 19) + 1;
+     // console.log(random);
+
+    var crystal = $("<div>");
+    crystal.attr( {
+      "class": "crystal",
+      "data-random": random
+    });
+    crystal.css({
+      "background-image":"url('" + images[i] + "')",
+       "background-size":"cover"
+  });
+    $(".crystals").append(crystal);
+  }
+  $("#previous").html("Total Score:" + previous);
+  }
+
+  resetAndStart();
+
+  var reset = function () {
+
+  }
+
+  //Event Delagation
+  $(document).on("click", ".crystal", function () {
+
+  var num = parseInt($(this).attr("data-random"));
+
+  $("#previous").html("Total Score:" + previous);
+  previous += num;
+  console.log(previous);
 
 
+  if(previous > random_number) {
+    loses++;
+    $("#loses").html("You lose:" + loses);
 
-score("New score: " + counter);
+    previous = 0;
 
-if (counter ===targetNumber) {
-catagory("you win!");
-}
+      $("#previous").html(previous);
+    resetAndStart();
+  }
 
-else if (counter > targetNumber) {
-catagory1("You lose!!");
+  else if(previous === random_number) {
+       win++;
+         $("#win").html("You Win:" + win);
 
+         previous = 0;
 
-}
-});
-
-});
+           $("#previous").html(previous);
+         resetAndStart();
+    }
+  });
